1b18b035ff0e3f071146153b306d41dee515b5bc
Harry <24208175+manister@users.noreply.github.com>
2023-10-06 12:42:29 +0100

    some random colours

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -17,28 +17,27 @@ export const options = {
 
 console.log(morningstarData.Portfolios[0].AssetAllocations) ;
 
+const {BreakdownValues} = morningstarData.Portfolios[0].AssetAllocations[0];
+
+
+const labels = BreakdownValues.map((item) => item.Type);
+
+const dataValues = BreakdownValues.map((item) => item.Value);
+
+
+const randomColourArray = Array.from({length: labels.length}).map(() => {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+})
+
+
+console.log(randomColourArray);
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: labels,
   datasets: [
     {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
+      label: 'percentage allocation',
+      data: dataValues,
+      backgroundColor: randomColourArray,
       borderWidth: 1,
     },
   ],
